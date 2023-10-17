b88811a068e354811de49d34717101d5f475f439
Name <email@exaple.ru>
2023-10-17 11:43:47 +0300

    final

--- WorkWithPatterns_2/Mem.cs
+++ WorkWithPatterns_2/Mem.cs
@@ -1,28 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using YoutubeExplode;
+using YoutubeExplode.Common;
+using YoutubeExplode.Converter;
 
 namespace WorkWithPatterns_2
 {
     public class Mem : Commands
     {
         YoutubeClient client;
-        public string Url = "https://youtu.be/gqRMSfWnCMc?si=ujtjWn212RCWAKic";
+        public string Url = "https://youtu.be/sc0kNKVmaek?si=Eavnw7QNjOeoVbDH";
         public Mem(YoutubeClient client)
         {
             this.client = client;
         }
-        public void Download()
+
+        public async void Download()
         {
             Console.WriteLine("Идет скачивание.");
+            await client.Videos.DownloadAsync(Url, "//Users//dima//Download");
         }
         public void ShowInfo()
         {
-            Console.WriteLine("Описание видео.");
-            client.Videos.GetAsync(Url);
+            var item = client.Videos.GetAsync(Url);
+            Console.WriteLine(item.Result);
         }
     }
 }
